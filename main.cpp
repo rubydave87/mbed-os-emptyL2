2866f883a717f7655b7898dc10be32d3c244ee21
rubydave87 <rubydave11@gmail.com>
2024-05-17 12:18:10 +0000

    com2
    Signed-off-by: rubydave87 <rubydave11@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include "mbed.h"
+#include <cstdio>
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+int max_value = 10;
 
+
+int fib(int n){
+    if(n == 0  || n == 1){
+        printf("*");
+        return n;
+    }
+    else {
+    printf("(%d), (%d)", n-1, n-2);
+        return (fib(n-1) + fib(n-2));
     }
 }
 
+int main()
+{
+    printf("lab 2 about to start!! \r\n");
+    printf("series is: ");
+   for (int i=0; i<max_value; i++){
+    printf("%d, ", fib(i));
+   }
+    printf("\r\n");
+
+   while (true) {
+ 
+   
+    }
+}
